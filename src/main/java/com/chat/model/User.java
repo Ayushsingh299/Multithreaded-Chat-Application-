20a5c395d64ecb6869ba3dd3984f39eb2d9cf761
Ayush Singh <ayush.24scse1420165@galgotiasuniversity.ac.in>
2025-11-20 22:50:40 +0530

    src/main/java/com/chat/model/User.java

---
+++ src/main/java/com/chat/model/User.java
@@ -0,0 +1,77 @@
+package com.chat.model;
+
+public class User {
+    private int userId;
+    private String username;
+    private String passwordHash;
+    private String email;
+    private String fullName;
+    private boolean isOnline;
+
+    public User() {}
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public void setOnline(boolean online) {
+        isOnline = online;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", isOnline=" + isOnline +
+                '}';
+    }
+}
